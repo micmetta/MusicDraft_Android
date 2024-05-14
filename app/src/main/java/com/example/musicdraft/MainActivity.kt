a8c21c01e8ed9062c1bf6d533c8d434b4b53de81
Pietro Sangermano <pietro.sangermano97@gmail.com>
2024-05-14 11:58:58 +0200

    prova di commit e push

--- app/src/main/java/com/example/musicdraft/MainActivity.kt
+++ app/src/main/java/com/example/musicdraft/MainActivity.kt
@@ -12,7 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.musicdraft.ui.theme.MusicDraftTheme
 
-// test commit
+// test committo1
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
