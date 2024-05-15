7c6bd970c5c41056b3f16f57990388315c3f8494
micmetta <michelemetta1997@gmail.com>
2024-05-15 11:22:43 +0200

    allineamento con master

--- app/src/main/java/com/example/musicdraft/ui/theme/Color.kt
+++ app/src/main/java/com/example/musicdraft/ui/theme/Color.kt
@@ -10,4 +10,5 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val GreenJC = Color(0xFF3FDC85) // colore barra
+val GreenJC = Color(0xFF3FDC85) // verde
+val BlueApp = Color(0xFF42A5F5) // celeste scuro
